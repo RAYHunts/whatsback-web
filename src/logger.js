7a41cba972fa44ba00232182321040b99ef65598
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-02-24 03:47:05 +0700

    refactor(logger): simplify logger configuration and remove redundant comments

--- src/logger.js
+++ src/logger.js
@@ -2,9 +2,6 @@ const path = require("node:path");
 const winston = require("winston");
 require("winston-daily-rotate-file");
 
-// Create a transport that rotates logs daily,
-// compresses the rotated logs, limits the size to 20MB,
-// and keeps logs for 14 days.
 const rotate = new winston.transports.DailyRotateFile({
   filename: path.join(__dirname, "..", "logs", "app-%DATE%.log"),
   datePattern: "YYYY-MM-DD",
@@ -18,20 +15,15 @@ const rotate = new winston.transports.DailyRotateFile({
   ),
 });
 
-// Create a Winston logger using the above rotate.
 const logger = winston.createLogger({
   level: "debug",
-  transports: [
-    rotate,
-  ],
+  transports: [rotate],
 });
 
-// Listen for errors in the rotation process.
 rotate.on("error", (error) => {
   logger.error("Error in log file rotation:", error);
 });
 
-// Optionally, listen for the 'rotate' event when a log file is rotated.
 rotate.on("rotate", (oldFilename, newFilename) => {
   logger.info(`Rotated log file: ${oldFilename} -> ${newFilename}`);
 });
