ab3772a9509eda04ea440d7f233daf9870fb1b5c
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-02-24 03:46:11 +0700

    feat(helper): add new utility functions and improve time zone handling
    - Added `toSnakeCase`, `calculateTypingDuration`, and `bannerCLI` utility functions.
    - Updated `serverLog` to support dynamic time zone configuration via `process.env.TZ`.
    - Refactored `formatInternationalPhoneNumber` for better readability using a `switch` statement.
    - Removed redundant comments in `removeDuplicateContacts` and `formatInternationalPhoneNumber`.
    - Exported new utility functions for use in other modules.
    
    This change enhances functionality and improves code maintainability.

--- src/helper.js
+++ src/helper.js
@@ -21,7 +21,7 @@ const serverLog = (type = "", message) => {
   }
 
   const now = new Date();
-  const options = { timeZone: "Asia/Jakarta", hour12: false };
+  const options = { timeZone: process.env.TZ || "UTC", hour12: false };
   const dateParts = now.toLocaleDateString("id-ID", options).split("/");
   const timeParts = now.toLocaleTimeString("id-ID", options).split(".");
   const formattedTimestamp = `${dateParts[2]}-${dateParts[1].padStart(
@@ -101,9 +101,7 @@ const phoneNumberFormatter = function (number) {
 const removeDuplicateContacts = (contacts) => {
   const uniqueContactsMap = new Map();
 
-  // Iterate through all contacts
   for (const contact of contacts) {
-    // Use the contact's number as the unique key
     if (
       !uniqueContactsMap.has(contact.number) &&
       uniqueContactsMap.id?.server === "c.us"
@@ -112,7 +110,6 @@ const removeDuplicateContacts = (contacts) => {
     }
   }
 
-  // Convert the Map back to an array
   return [...uniqueContactsMap.values()];
 };
 
@@ -123,37 +120,35 @@ const removeDuplicateContacts = (contacts) => {
  */
 const formatInternationalPhoneNumber = (number) => {
   number = number.toString();
-  // Ensure the number starts with "62" (Indonesia's country code)
+
   if (!number.startsWith("62")) {
     return number;
   }
 
-  // Remove the country code and add the plus sign
-  let localNumber = number.slice(2); // Remove "62"
+  let localNumber = number.slice(2);
 
-  // Format based on number length (handling 10-15 digit local numbers)
   switch (localNumber.length) {
-  case 10: {
-    return `+62 ${localNumber.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3")}`;
-  }
-  case 11: {
-    return `+62 ${localNumber.replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3")}`;
-  }
-  case 12: {
-    return `+62 ${localNumber.replace(/(\d{4})(\d{4})(\d{4})/, "$1-$2-$3")}`;
-  }
-  case 13: {
-    return `+62 ${localNumber.replace(/(\d{4})(\d{4})(\d{5})/, "$1-$2-$3")}`;
-  }
-  case 14: {
-    return `+62 ${localNumber.replace(/(\d{4})(\d{5})(\d{5})/, "$1-$2-$3")}`;
-  }
-  case 15: {
-    return `+62 ${localNumber.replace(/(\d{4})(\d{5})(\d{6})/, "$1-$2-$3")}`;
-  }
-  default: {
-    return "Invalid length for an Indonesian number";
-  }
+    case 10: {
+      return `+62 ${localNumber.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3")}`;
+    }
+    case 11: {
+      return `+62 ${localNumber.replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3")}`;
+    }
+    case 12: {
+      return `+62 ${localNumber.replace(/(\d{4})(\d{4})(\d{4})/, "$1-$2-$3")}`;
+    }
+    case 13: {
+      return `+62 ${localNumber.replace(/(\d{4})(\d{4})(\d{5})/, "$1-$2-$3")}`;
+    }
+    case 14: {
+      return `+62 ${localNumber.replace(/(\d{4})(\d{5})(\d{5})/, "$1-$2-$3")}`;
+    }
+    case 15: {
+      return `+62 ${localNumber.replace(/(\d{4})(\d{5})(\d{6})/, "$1-$2-$3")}`;
+    }
+    default: {
+      return "Invalid length for an Indonesian number";
+    }
   }
 };
 
@@ -165,13 +160,110 @@ const formatInternationalPhoneNumber = (number) => {
  */
 const parseOrigins = (value) => {
   if (!value || typeof value !== "string") return "*";
-  // Split on comma, trim spaces, and filter out any empty entries
+
   const origins = value
     .split(",")
     .map((o) => o.trim())
     .filter((o) => o.length > 0);
-  // Return the array if any valid entries exist, otherwise default to "*"
+
   return origins.length > 0 ? origins : "*";
+};
+
+/**
+ * Converts a given string to snake_case.
+ * 
+ * This function replaces spaces and uppercase letters with underscores, removes
+ * non-word characters, and converts the entire string to lowercase.
+ *
+ * @param {string} text - The input string to be converted.
+ * @returns {string} - The converted snake_case string.
+ */
+
+const toSnakeCase = (text) => {
+  return text
+    .replace(/([a-z])([A-Z])/g, "$1_$2")
+    .replace(/\s+/g, "_")
+    .replace(/[^\w]/g, "")
+    .toLowerCase();
+};
+
+  /**
+   * Calculates the duration of a typing simulation based on the length of the input message.
+   *
+   * The calculation takes into account the average typing speed, the minimum and maximum delay,
+   * and introduces a random variation to simulate human behavior. For longer messages, the
+   * function adds a pause duration to simulate the natural pauses between words.
+   *
+   * @param {string} message - The input message to calculate the typing duration for.
+   * @returns {number} - The calculated typing duration in milliseconds.
+   */
+const calculateTypingDuration = (message) => {
+  const config = {
+    avgSpeed: 225,
+    minDelay: 800,
+    maxDelay: 12000,
+    speedVariation: 0.3,
+    humanPauseThreshold: 75,
+    pauseDuration: 1200,
+  };
+
+  let baseTime = (message.length / config.avgSpeed) * 60 * 1000;
+
+  const speedVariation = 1 + (Math.random() * 2 - 1) * config.speedVariation;
+  baseTime *= speedVariation;
+
+  if (message.length > config.humanPauseThreshold) {
+    const pauseCount = Math.floor(message.length / config.humanPauseThreshold);
+    baseTime += pauseCount * config.pauseDuration;
+  }
+
+  return Math.min(config.maxDelay, Math.max(config.minDelay, baseTime));
+};
+
+/**
+ * Prints a banner to the console with information about the running
+ * Whatsback server instance.
+ *
+ * The banner includes the title, port number, REST API URL, Whatsback UI URL,
+ * and links to support the project via GitHub Sponsors and Saweria.
+ *
+ * @param {number} PORT - The port number that the server is listening on.
+ */
+const bannerCLI = async (PORT) => {
+  const { default: boxen } = await import("boxen");
+
+  const title = `Whatsback Server is Running`;
+  const maxLength = Math.max(
+    title.length,
+    38 + PORT.toString().length,
+    "[Support via GitHub Sponsors]".length + 23,
+    "[Support via Saweria]".length + 15
+  );
+  const separator = "─".repeat(maxLength + 2);
+
+  const UI_PORT =
+    process.env.NODE_ENV === "production" ? process.env.UI_PORT || 8169 : PORT;
+
+  const text = [
+    `\x1b[0m${title}\x1b[0m`,
+    `\x1b[0m${separator}\x1b[0m`,
+    `\x1b[0m📡 Running on: \x1b[1;35mhttp://localhost:${PORT}\x1b[0m`,
+    `\x1b[0m🔌 REST API:   \x1b[1;32mhttp://localhost:${PORT}\x1b[0m`,
+    `\x1b[0m💻 Whatsback UI: \x1b[1;32mhttp://localhost:${UI_PORT}\x1b[0m`,
+    `\x1b[0m${separator}\x1b[0m`,
+    `\x1b[0m💖 Support: \x1b[4;36mhttps://github.com/sponsors/darkterminal\x1b[0m`,
+    `\x1b[0m💖 Support: \x1b[4;36mhttps://saweria.co/darkterminal\x1b[0m`,
+  ].join("\n");
+
+  const boxedBanner = boxen(text, {
+    padding: 1,
+    margin: 1,
+    borderStyle: "round",
+    borderColor: "cyan",
+    align: "center",
+  });
+
+  console.log(boxedBanner);
 };
 
 module.exports = {
@@ -181,4 +273,7 @@ module.exports = {
   removeDuplicateContacts,
   formatInternationalPhoneNumber,
   parseOrigins,
+  toSnakeCase,
+  calculateTypingDuration,
+  bannerCLI,
 };
